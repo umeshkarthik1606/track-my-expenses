a374800bdc01a31c023876c8659a00262b7e62d5
Umesh Karthik <umeshkarthik1606@gmail.com>
2026-02-27 22:01:51 +0530

    Update UI theme and footer content

--- expense/src/components/common/footer/index.js
+++ expense/src/components/common/footer/index.js
@@ -5,17 +5,17 @@ const Footer = () => {
   return (
     <div className="footer">
       <h4>
-       Made  by{" "}
+       {" "}
         <a
           href="https://github.com/himanshuchandola"
           className="author-name"
           target="_blank"
           rel="noreferrer"
         >
-          Himanshu Chandola
+          
         </a>{" "}
-        as{" "}
-          MCA 4th Sem Project
+        {" "}
+          
       </h4>
     </div>
   );
